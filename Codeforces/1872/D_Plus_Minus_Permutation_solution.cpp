3a103739195445068b42a5061649a24aab78cd7e
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-03-08 00:48:21 +0530

    Add Plus Minus Permutation [D] from Codeforces

---
+++ Codeforces/1872/D_Plus_Minus_Permutation_solution.cpp
@@ -0,0 +1,38 @@
+// Problem: Plus Minus Permutation
+// Contest: 1872
+// Link: https://codeforces.com/contest/1872/problem/D
+// Submission id: 365769412
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+ 
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t;
+    cin >> t;
+ 
+    while(t--){
+        ll n,x,y;
+        cin >> n >> x >> y;
+ 
+        ll l = lcm(x,y);
+ 
+        ll a = n/x;
+        ll b = n/y;
+        ll c = n/l;
+ 
+        ll p = a - c;
+        ll q = b - c;
+ 
+        ll s1 = p*(2*n - p + 1)/2;
+        ll s2 = q*(q + 1)/2;
+ 
+        cout << s1 - s2 << "\n";
+    }
+ 
+    return 0;
+}
