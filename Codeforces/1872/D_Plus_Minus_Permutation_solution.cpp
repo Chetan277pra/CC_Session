c670939f738be1e27435eb1bc977e4e538a009dc
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-03-08 01:12:46 +0530

    Add Plus Minus Permutation [D] from Codeforces

--- Codeforces/1872/D_Plus_Minus_Permutation_solution.cpp
+++ Codeforces/1872/D_Plus_Minus_Permutation_solution.cpp
@@ -5,35 +5,35 @@
 
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 #define ll long long
- 
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
- 
+
     int t;
     cin >> t;
- 
+
     while(t--){
         ll n,x,y;
         cin >> n >> x >> y;
- 
+
         ll l = lcm(x,y);
- 
+
         ll a = n/x;
         ll b = n/y;
         ll c = n/l;
- 
+
         ll p = a - c;
         ll q = b - c;
- 
+
         ll s1 = p*(2*n - p + 1)/2;
         ll s2 = q*(q + 1)/2;
- 
+
         cout << s1 - s2;
         cout<<"\n";
     }
- 
+
     return 0;
 }
