44ea5d5113e4f559636deac6d3153e6ffc870d57
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-03-09 00:27:36 +0530

    Add 1-1 [A]

---
+++ Codeforces/2207/A_1-1_solution.cpp
@@ -0,0 +1,39 @@
+// Problem: 1-1
+// Contest: 2207
+// Link: https://codeforces.com/contest/2207/problem/A
+// Submission id: 365908359
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int tt;
+    cin >> tt;
+    while(tt--){
+      int n;
+      cin >> n;
+      string s;
+      cin >> s;
+      for(int i = 1; i < n-1 ; i++){
+        if(s[i-1]=='1' and s[i]=='0' and s[i+1]=='1') s[i] = '1';
+      }
+      int maxi = 0;
+      for(auto a : s) if(a == '1' ) maxi++;
+      int mini = 0;
+      int curr = 0;
+      // cout<<s<<endl;
+      for(int i = 0;i < n; i++){
+        if(s[i] == '0'){
+          mini += curr - (curr-1)/2;
+          curr = 0;
+        }
+        else curr++;
+      }
+      mini += curr - (curr-1)/2;
+      
+      cout<<mini<<" "<<maxi;
+      cout << "\n";
+    }
+    return 0;
+}
