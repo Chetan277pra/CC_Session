1475eccbb830483a99c43700169b219aa089ff26
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-03-31 19:51:43 +0530

    Add Are You a Robot? [A]

---
+++ Codeforces/1812/A_Are_You_a_Robot_solution.cpp
@@ -0,0 +1,13 @@
+// Problem: Are You a Robot?
+// Contest: 1812
+// Link: https://codeforces.com/contest/1812/problem/A
+// Submission id: 368882515
+
+#include <iostream>
+using namespace std;
+ 
+int main() 
+{
+    cout << "security";
+    return 0;
+}
