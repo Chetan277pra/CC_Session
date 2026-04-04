08bbb17c5bf9555130c10c3186e2e145e3e84e6f
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-04-04 20:10:24 +0530

    Add The 67th Integer Problem [A]

---
+++ Codeforces/2218/A_The_67th_Integer_Problem_solution.cpp
@@ -0,0 +1,19 @@
+// Problem: The 67th Integer Problem
+// Contest: 2218
+// Link: https://codeforces.com/contest/2218/problem/A
+// Submission id: 369609682
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int tt;
+    cin >> tt;
+    while(tt--){
+        int n;
+        cin >> n;
+        // vector<int> vec(n);
+        // for(auto & a : vec) cin >> a;
+        cout<<67<<"\n";
+    }
+    return 0;
+}
