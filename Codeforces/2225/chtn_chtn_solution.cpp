08146c5f072c10e650b6fdae09825088f4a38987
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-04-25 19:12:56 +0530

    Add chtn

--- Codeforces/2225/chtn_chtn_solution.cpp
+++ Codeforces/2225/chtn_chtn_solution.cpp
@@ -1,22 +1,27 @@
 // Problem: chtn
 // Contest: 2225
-// Submission id: 372366049
+// Submission id: 372469039
 
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
- 
-int32_t main() {
+int32_t main() 
+{
     int tt;
     cin >> tt;
-    while(tt--) {
-        int x , y;
-        cin >> x >> y;
-        
-        if (y == 2 * x) 
-            cout << "NO\n";
-        else 
-            cout << "YES\n";
+    while(tt--){
+        string s;
+        cin >> s;
+        int two = 0;
+
+        int n = s.length();
+        int temp = 1;
+        for(int i = 1; i < n; i++){
+            if(s[i] == s[i-1]) two+=1; 
+        }
+
+        if(two > 2) cout<<"NO\n";
+        else cout<<"YES\n"; 
     }
     return 0;
 }
