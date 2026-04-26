a08fa03b1e9305e20fe8721d7771d254f6982525
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-04-26 21:24:09 +0530

    Add chtn

--- Codeforces/2225/chtn_chtn_solution.cpp
+++ Codeforces/2225/chtn_chtn_solution.cpp
@@ -1,27 +1,44 @@
 // Problem: chtn
 // Contest: 2225
-// Submission id: 372469039
+// Submission id: 372631542
 
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-int32_t main() 
-{
-    int tt;
-    cin >> tt;
-    while(tt--){
-        string s;
-        cin >> s;
-        int two = 0;
-
-        int n = s.length();
-        int temp = 1;
-        for(int i = 1; i < n; i++){
-            if(s[i] == s[i-1]) two+=1; 
-        }
-
-        if(two > 2) cout<<"NO\n";
-        else cout<<"YES\n"; 
+int n;
+string a, b;
+vector<int> dp;
+ 
+int rec(int i){
+    if(i < 0) return 0;
+ 
+    if(dp[i] != -1) return dp[i];
+ 
+    int uppar = (a[i] != b[i]) + rec( i-1 );
+ 
+    int sidha = INT_MAX;
+    if(i > 0){
+        int cost = 0;
+        if(a[i] != a[i-1]) cost++;
+        if(b[i] != b[i-1]) cost++;
+        sidha = cost + rec( i-2 );
     }
+    return dp[i] = min( uppar , sidha);
+}
+ 
+int32_t main() {
+ 
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int tt;
+    cin >> tt; 
+    while (tt--) {
+        cin >> n;
+        cin >> a >> b;
+        dp.assign(n , -1);
+        cout << rec(n-1) << '\n';
+    }
+ 
     return 0;
 }
