dce67f32a0a88a30fd3830407d14ae63e23e1d52
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-04-27 21:48:39 +0530

    Add Palindromifier [C]

---
+++ Codeforces/1421/C_Palindromifier_solution.cpp
@@ -0,0 +1,19 @@
+// Problem: Palindromifier
+// Contest: 1421
+// Link: https://codeforces.com/contest/1421/problem/C
+// Submission id: 372733893
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    string s;
+    cin >> s;
+    int n = s.length();
+    cout<<3<<"\n";
+    cout<<"R "<<n-1<<"\n";
+    cout<<"L "<<n<<"\n";
+    cout<<"L 2"<<"\n";
+    return 0;
+}
