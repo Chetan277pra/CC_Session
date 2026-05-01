48869cd58e3fefd5a7c7abbc983d7ae07923501a
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-05-01 19:36:08 +0530

    Add Koshary [A]

---
+++ Codeforces/2227/A_Koshary_solution.cpp
@@ -0,0 +1,21 @@
+// Problem: Koshary
+// Contest: 2227
+// Link: https://codeforces.com/contest/2227/problem/A
+// Submission id: 373236028
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() 
+{
+    int tt;
+    cin >> tt;
+    while(tt--){
+        int a , b;
+        cin >> a >> b;
+        if(a%2 == 1 and b%2 == 1) cout<<"NO\n";
+        else cout<<"YES\n";
+ 
+    }
+    return 0;
+}
