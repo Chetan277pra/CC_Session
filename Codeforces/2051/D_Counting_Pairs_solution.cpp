392776c318563e20523eb646fcdfa3a7c5711a0c
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-06-08 21:15:03 +0530

    Add Counting Pairs [D]

--- Codeforces/2051/D_Counting_Pairs_solution.cpp
+++ Codeforces/2051/D_Counting_Pairs_solution.cpp
@@ -1,43 +1,40 @@
 // Problem: Counting Pairs
 // Contest: 2051
 // Link: https://codeforces.com/contest/2051/problem/D
-// Submission id: 377264200
+// Submission id: 377806447
 
 #include<bits/stdc++.h>
 using namespace std;
-#define int long long
-int32_t main(){
-    int tt;
-    cin >> tt;
-    while(tt--){
-        int n , x , y;
-        cin >> n >> x >> y;
-        vector<int> vec(n);
-        for(auto & a : vec) cin >> a;
-        int ans = 0;
-        // tot - a - rem > x
-        // tot - a - x > rem;
-        int tot = accumulate(vec.begin() , vec.end() , 0ll);
-        sort(vec.begin() , vec.end());
-        for(int i = 0; i < n; i++){
-            int rem1 = tot - x - vec[i];
-            auto it1 = upper_bound(vec.begin() , vec.end() , rem1);
-            int rem2 = tot - y - vec[i];
-            auto it2 = lower_bound(vec.begin() , vec.end() , rem2);
-            // cout << vec[i]<<" " << *it1 <<" "<<*it2 << " ";
-            // cout << it1 - it2 << endl;
-            int ind1 = it1 - vec.begin();
-            int ind2 = it2 - vec.begin();
-            if(ind2 <= i && i < ind1) ans--;
-            // ans = max(ans , 0);
-            ans += (it1 - it2 );
-        }
+using ll=long long;
 
-        cout << ans/2<<"\n";
+ll f(vector<ll>&a,ll k){
+    int i=0,j=a.size()-1;
+    ll c=0;
+    while(i<j){
+        if(a[i]+a[j]<=k){
+            c+=j-i;
+            i++;
+        }else j--;
     }
-    return 0;
+    return c;
 }
-// 19
-// 3 4 6 6
-// 19 - 3 - x > 8   < y
-// 19 - 3 - x < 10 == -x < -6 x >= 6
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        ll x,y,s=0;
+        cin>>n>>x>>y;
+        vector<ll>a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+            s+=a[i];
+        }
+        sort(a.begin(),a.end());
+        ll l=s-y,r=s-x;
+        cout<<f(a,r)-f(a,l-1)<<"\n";
+    }
+}
