54f048da39a3c022a68e2082b09c9d853367b882
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-06-27 09:53:18 +0530

    Add Dora and Search [C]

---
+++ Codeforces/1793/C_Dora_and_Search_solution.cpp
@@ -0,0 +1,52 @@
+// Problem: Dora and Search
+// Contest: 1793
+// Link: https://codeforces.com/contest/1793/problem/C
+// Submission id: 380230257
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+ 
+        int l = 0, r = n - 1;
+        int mn = 1, mx = n;
+ 
+        while (l < r) {
+            if (a[l] == mn) {
+                l++;
+                mn++;
+            } else if (a[l] == mx) {
+                l++;
+                mx--;
+            } else if (a[r] == mn) {
+                r--;
+                mn++;
+            } else if (a[r] == mx) {
+                r--;
+                mx--;
+            } else {
+                break;
+            }
+        }
+ 
+        if (l >= r)
+            cout << -1 << '\n';
+        else
+            cout << l + 1 << " " << r + 1 << '\n';
+    }
+ 
+    return 0;
+}
