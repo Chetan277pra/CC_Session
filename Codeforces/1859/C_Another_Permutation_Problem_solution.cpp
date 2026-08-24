0565998e0d4e054be2f2c1eba42c0ee638040ebf
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-08-24 22:32:08 +0530

    Add Another Permutation Problem [C]

--- Codeforces/1859/C_Another_Permutation_Problem_solution.cpp
+++ Codeforces/1859/C_Another_Permutation_Problem_solution.cpp
@@ -5,34 +5,34 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main() {
     int t;
     cin >> t;
-
+ 
     while (t--) {
         int n;
         cin >> n;
-
+ 
         long long ans = 0;
-
+ 
         for (int k = 0; k <= n; k++) {
             long long sum = 0, mx = 0;
-
+ 
             for (int i = 1; i <= k; i++) {
                 sum += 1LL * i * i;
                 mx = max(mx, 1LL * i * i);
             }
-
+ 
             for (int i = k + 1; i <= n; i++) {
                 int x = n + k + 1 - i;
                 sum += 1LL * i * x;
                 mx = max(mx, 1LL * i * x);
             }
-
+ 
             ans = max(ans, sum - mx);
         }
-
+ 
         cout << ans << '\n';
     }
 }
