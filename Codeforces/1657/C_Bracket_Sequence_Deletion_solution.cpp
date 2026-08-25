3d87720c6af84ff5f220db13be5418444b068447
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-08-25 21:54:04 +0530

    Add Bracket Sequence Deletion [C]

---
+++ Codeforces/1657/C_Bracket_Sequence_Deletion_solution.cpp
@@ -0,0 +1,47 @@
+// Problem: Bracket Sequence Deletion
+// Contest: 1657
+// Link: https://codeforces.com/contest/1657/problem/C
+// Submission id: 388376669
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tt;
+    cin >> tt;
+
+    while (tt--) {
+        int n;
+        string s;
+        cin >> n >> s;
+
+        int p = 0, a = 0;
+
+        while (p < n) {
+            if (s[p] == '(') {
+                if (p + 1 >= n) break;
+                p += 2;
+                a++;
+            } else {
+                int j = p + 1;
+
+                while (j < n && s[j] != ')')
+                    j++;
+
+                if (j == n) break;
+
+                p = j + 1;
+                a++;
+            }
+        }
+
+        cout << a << " " << n - p << '\n';
+    }
+
+    return 0;
+}
