2e72dc5c1c19751225108fcb46684d6fe34a17e3
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-08-26 21:51:20 +0530

    Add chtn

---
+++ Codeforces/1916/chtn_chtn_solution.cpp
@@ -0,0 +1,41 @@
+// Problem: chtn
+// Contest: 1916
+// Submission id: 388487362
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int sum = 0;
+        int odd = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num;
+            cin >> num;
+
+            sum += num;
+            odd += num & 1;
+
+            int sub = odd / 3;
+
+            if (odd % 3 == 1 && i != 0)
+                sub++;
+
+            cout << sum - sub << (i == n - 1 ? '\n' : ' ');
+        }
+    }
+
+    return 0;
+}
