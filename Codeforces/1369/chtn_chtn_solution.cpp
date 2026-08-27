1085a8ded8f8bf4543e1147a2e6a30aa6329f499
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-08-27 21:15:54 +0530

    Add chtn

---
+++ Codeforces/1369/chtn_chtn_solution.cpp
@@ -0,0 +1,36 @@
+// Problem: chtn
+// Contest: 1369
+// Submission id: 388589539
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        string s;
+        cin >> n >> s;
+
+        int l = 0, r = n - 1;
+
+        while (l < n && s[l] == '0')
+            l++;
+
+        while (r >= 0 && s[r] == '1')
+            r--;
+
+        if (l > r) {
+            cout << s << '\n';
+        } else {
+            cout << s.substr(0, l) << '0' << s.substr(r + 1) << '\n';
+        }
+    }
+
+    return 0;
+}
