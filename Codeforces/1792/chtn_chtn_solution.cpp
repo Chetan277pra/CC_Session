90e41b62d7ae04b82e9673b0a0d06d5dad78a4b0
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-08-28 22:37:50 +0530

    Add chtn

---
+++ Codeforces/1792/chtn_chtn_solution.cpp
@@ -0,0 +1,39 @@
+// Problem: chtn
+// Contest: 1792
+// Submission id: 388701036
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long a, b, c, d;
+        cin >> a >> b >> c >> d;
+
+        if (a == 0) {
+            cout << 1 << '\n';
+            continue;
+        }
+
+        long long x = min(b, c);
+        long long ans = a + 2 * x;
+
+        b -= x;
+        c -= x;
+
+        long long r = b + c + d;
+
+        ans += min(a, r);
+
+        if (r > a)
+            ans++;
+
+        cout << ans << '\n';
+    }
+}
