082ae1a5efd30aef170f23f19980dfac593a5100
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-08-28 22:38:03 +0530

    Add AccurateLee [B]

--- Codeforces/1369/B_AccurateLee_solution.cpp
+++ Codeforces/1369/B_AccurateLee_solution.cpp
@@ -5,33 +5,33 @@
 
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
- 
+
     int t;
     cin >> t;
- 
+
     while (t--) {
         int n;
         string s;
         cin >> n >> s;
- 
+
         int l = 0, r = n - 1;
- 
+
         while (l < n && s[l] == '0')
             l++;
- 
+
         while (r >= 0 && s[r] == '1')
             r--;
- 
+
         if (l > r) {
             cout << s << '\n';
         } else {
             cout << s.substr(0, l) << '0' << s.substr(r + 1) << '\n';
         }
     }
- 
+
     return 0;
 }
