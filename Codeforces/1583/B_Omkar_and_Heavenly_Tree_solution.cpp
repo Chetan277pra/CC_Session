b2fba319062be7ac674be47be3704faed7720c91
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-08-30 23:03:37 +0530

    Add Omkar and Heavenly Tree [B]

--- Codeforces/1583/B_Omkar_and_Heavenly_Tree_solution.cpp
+++ Codeforces/1583/B_Omkar_and_Heavenly_Tree_solution.cpp
@@ -5,34 +5,34 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
+ 
     int t;
     cin >> t;
-
+ 
     while (t--) {
         int n, m;
         cin >> n >> m;
-
+ 
         vector<int> v(n + 1, 0);
-
+ 
         for (int i = 0; i < m; i++) {
             int a, b, c;
             cin >> a >> b >> c;
             v[b] = 1;
         }
-
+ 
         int r = 1;
         while (v[r]) r++;
-
+ 
         for (int i = 1; i <= n; i++) {
             if (i != r)
                 cout << r << " " << i << '\n';
         }
     }
-
+ 
     return 0;
 }
