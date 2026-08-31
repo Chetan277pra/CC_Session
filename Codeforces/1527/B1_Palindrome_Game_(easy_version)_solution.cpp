2bec1749594c057e4dd7229b7b5f2dbdc9a07cf1
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-08-31 22:53:37 +0530

    Add Palindrome Game (easy version) [B1]

--- Codeforces/1527/B1_Palindrome_Game_(easy_version)_solution.cpp
+++ Codeforces/1527/B1_Palindrome_Game_(easy_version)_solution.cpp
@@ -5,37 +5,37 @@
 
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 void solve() {
     int n;
     string s;
     cin >> n >> s;
- 
+
     int a = 0, b = 0;
     int diff = 0, same = 0, same2 = 0;
- 
+
     for (int i = 0; i < n / 2; i++) {
         if (s[i] != s[n - 1 - i])
             diff++;
- 
+
         else if (s[i] == '0')
             same++;
     }
- 
+
     if (n % 2 && s[n / 2] == '0')
         same2 = 1;
- 
+
     if (diff == 0) {
         if (same > 0 && same2)
             cout << "ALICE\n";
         else
             cout << "BOB\n";
- 
+
         return;
     }
- 
+
     b = diff;
- 
+
     if (!same2) {
         a += ((same + 1) / 2) * 2;
         b += (same / 2) * 2;
@@ -45,7 +45,7 @@ void solve() {
         b += ((same + 1) / 2) * 2;
         a += (same / 2) * 2;
     }
- 
+
     if (a < b)
         cout << "ALICE\n";
     else if (a > b)
@@ -53,14 +53,14 @@ void solve() {
     else
         cout << "DRAW\n";
 }
- 
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
- 
+
     int tt;
     cin >> tt;
- 
+
     while (tt--)
         solve();
 }
