62b7f91e1a05670961da674f7adf25a514302303
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-02 22:11:46 +0530

    Add chtn

---
+++ Codeforces/1343/chtn_chtn_solution.cpp
@@ -0,0 +1,38 @@
+// Problem: chtn
+// Contest: 1343
+// Submission id: 389207446
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt;
+    cin >> tt;
+
+    while (tt--) {
+        int n;
+        cin >> n;
+
+        vector<long long> a(n);
+        for (auto &x : a) cin >> x;
+
+        long long ans = 0;
+
+        for (int i = 0; i < n; ) {
+            long long mx = a[i];
+            int j = i + 1;
+
+            while (j < n && ((a[j] > 0) == (a[i] > 0))) {
+                mx = max(mx, a[j]);
+                j++;
+            }
+
+            ans += mx;
+            i = j;
+        }
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
