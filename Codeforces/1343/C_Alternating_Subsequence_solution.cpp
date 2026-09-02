c57acd3ff432d1121bbda2e423432e4786ec7b9b
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-02 22:32:08 +0530

    Add Alternating Subsequence [C]

--- Codeforces/1343/C_Alternating_Subsequence_solution.cpp
+++ Codeforces/1343/C_Alternating_Subsequence_solution.cpp
@@ -5,35 +5,35 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main() {
     int tt;
     cin >> tt;
-
+ 
     while (tt--) {
         int n;
         cin >> n;
-
+ 
         vector<long long> a(n);
         for (auto &x : a) cin >> x;
-
+ 
         long long ans = 0;
-
+ 
         for (int i = 0; i < n; ) {
             long long mx = a[i];
             int j = i + 1;
-
+ 
             while (j < n && ((a[j] > 0) == (a[i] > 0))) {
                 mx = max(mx, a[j]);
                 j++;
             }
-
+ 
             ans += mx;
             i = j;
         }
-
+ 
         cout << ans << '\n';
     }
-
+ 
     return 0;
 }
