e3ac09c5a18fa6b1f8809496e9a04a5a765b83d2
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-05 20:19:57 +0530

    Add The Strict Teacher (Hard Version) [B2]

---
+++ Codeforces/2005/B2_The_Strict_Teacher_(Hard_Version)_solution.cpp
@@ -0,0 +1,48 @@
+// Problem: The Strict Teacher (Hard Version)
+// Contest: 2005
+// Link: https://codeforces.com/contest/2005/problem/B2
+// Submission id: 389471201
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, m, q;
+        cin >> n >> m >> q;
+
+        vector<int> b(m);
+
+        for (int &x : b)
+            cin >> x;
+
+        sort(b.begin(), b.end());
+
+        while (q--) {
+            int x;
+            cin >> x;
+
+            int pos = lower_bound(b.begin(), b.end(), x) - b.begin();
+
+            if (pos == 0) {
+                cout << b[0] - 1 << '\n';
+            }
+            else if (pos == m) {
+                cout << n - b[m - 1] << '\n';
+            }
+            else {
+                cout << (b[pos] - b[pos - 1]) / 2 << '\n';
+            }
+        }
+    }
+
+    return 0;
+}
