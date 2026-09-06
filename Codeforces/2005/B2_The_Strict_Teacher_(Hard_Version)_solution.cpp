d324b0110bc0b7f7080a9896f09e7bc5268762c5
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-06 11:35:45 +0530

    Add The Strict Teacher (Hard Version) [B2]

--- Codeforces/2005/B2_The_Strict_Teacher_(Hard_Version)_solution.cpp
+++ Codeforces/2005/B2_The_Strict_Teacher_(Hard_Version)_solution.cpp
@@ -3,46 +3,64 @@
 // Link: https://codeforces.com/contest/2005/problem/B2
 // Submission id: 389471201
 
-#include <bits/stdc++.h>
+#include "codeforces.h"
+#include <chrono>
+#include <vector>
+
 using namespace std;
+using namespace std::chrono_literals;
+using namespace cf;
 
-#define int long long
+int main() {
+    Blog announcement("Codeforces Round 1119 (Div. 3)");
 
-int32_t main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    announcement << "Hello Codeforces!\n";
+    announcement << "I am delighted to invite you to participate in " << Codeforces Round 1119 (Div. 3) << "!\n";
+    announcement << "The problems were written and prepared by " << nik_exists << ".\n\n";
 
-    int t;
-    cin >> t;
+    announcement << ContestInfo {
+        .start_time = Saturday, September 5, 2026 at 20:15UTC+5.5,
+        .duration = 2h + 15min,
+        .number_of_problems = 7,
+        .subtasks = true // possibly not adjacent
+    };
 
-    while (t--) {
-        int n, m, q;
-        cin >> n >> m >> q;
+    announcement << RatingRules {
+        .rated_for_rating_below = 1600,
+        .unrated_registration_allowed = true,
+        .trusted_max_rating_below = 1900,
+        .trusted_min_rated_rounds = 5
+    };
 
-        vector<int> b(m);
+    announcement << JudgingRules {
+        .scoring = codeforces::ICPC,
+        .wrong_answer_penalty = 10min,
+        .open_hacking_phase = 12h,
+        .final_tests = after_open_hacks
+    };
 
-        for (int &x : b)
-            cin >> x;
+    auto is_cheater = [](User user) {
+        // https://codeforces.com/blog/entry/133941
+        if (user.used_ai_in_contest()) return true;
+        if (user.discussed_problems_online_before_contest_end()) return true;
+        if (user.violated_other_rules_im_too_lazy_to_list()) return true;
+        return false;
+    };
 
-        sort(b.begin(), b.end());
-
-        while (q--) {
-            int x;
-            cin >> x;
-
-            int pos = lower_bound(b.begin(), b.end(), x) - b.begin();
-
-            if (pos == 0) {
-                cout << b[0] - 1 << '\n';
-            }
-            else if (pos == m) {
-                cout << n - b[m - 1] << '\n';
-            }
-            else {
-                cout << (b[pos] - b[pos - 1]) / 2 << '\n';
-            }
-        }
+    for (auto user : get_users(2259)) {
+        if (is_cheater(user)) user.send_to_cry_basement();
     }
 
-    return 0;
+    announcement << Acknowledgments {
+        .coordinator = cry,
+        .russian_translation = Vladosiya,
+        .red_testers = {__baozii__, AksLolCoding, awesomeguy856, nifeshe},
+        .golden_testers = {Arpa, Intellegent, Proof_by_QED, temporary1},
+        .purple_testers = {Argentum47, Euclid73, Jteh, Lilypad, SpyrosAliv, nimoxide, omsincoconut, wakanda-forever, yse},
+        .blue_testers = {CatsAreCool, Vladosiya, chromate00, fatespeaker, linearspace, reirugan, simplelife},
+        .green_testers = {hannah12345},
+        .codeforces_and_polygon = {KAN, MikeMirzayanov}
+    };
+
+    announcement << "\nBest of luck, and thank y'all so much for competing!\n";
 }
