f399c5bb599044dae6d96a4b2f240ea8ffb73fb4
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-07 11:11:18 +0530

    Add A-B-C Sort [D]

--- Codeforces/1674/D_A-B-C_Sort_solution.cpp
+++ Codeforces/1674/D_A-B-C_Sort_solution.cpp
@@ -3,10 +3,39 @@
 // Link: https://codeforces.com/contest/1674/problem/D
 // Submission id: 389671906
 
-3
-4
-3 1 5 3
-3
-3 2 1
-1
-7331
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        for (int i = n - 2; i >= 0; i -= 2) {
+            if (a[i] > a[i + 1]) {
+                swap(a[i], a[i + 1]);
+            }
+        }
+
+        if (is_sorted(a.begin(), a.end())) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
