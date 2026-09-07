1a30a4e82890adc8dfd7bb8820c58f392c4e6fb9
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-07 12:17:34 +0530

    Add Basil's Garden [C]

--- Codeforces/1987/C_Basil's_Garden_solution.cpp
+++ Codeforces/1987/C_Basil's_Garden_solution.cpp
@@ -5,9 +5,9 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 #define int long long
-
+ 
 int32_t main() {
     
     int tt;
@@ -31,6 +31,6 @@ int32_t main() {
     
         cout << time << '\n';
     }
-
+ 
     return 0;
 }
