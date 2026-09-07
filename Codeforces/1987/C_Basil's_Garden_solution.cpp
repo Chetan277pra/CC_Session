e9876c2bde64fafdfa9e74bfc5bda35a54266bea
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-07 11:36:16 +0530

    Add Basil's Garden [C]

---
+++ Codeforces/1987/C_Basil's_Garden_solution.cpp
@@ -0,0 +1,36 @@
+// Problem: Basil's Garden
+// Contest: 1987
+// Link: https://codeforces.com/contest/1987/problem/C
+// Submission id: 389711710
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+int32_t main() {
+    
+    int tt;
+    cin >> tt;
+    while(tt--){
+        
+        int n;
+        cin >> n;
+    
+        vector<int> h(n);
+    
+        for (int i = 0; i < n; i++) {
+            cin >> h[i];
+        }
+    
+        int time = h[n - 1];
+    
+        for (int i = n - 2; i >= 0; i--) {
+            time = max(h[i], time + 1);
+        }
+    
+        cout << time << '\n';
+    }
+
+    return 0;
+}
