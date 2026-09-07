f93b76be3f6edf52306647ae615fd17368f3d831
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-07 11:38:47 +0530

    Add chtn

---
+++ Codeforces/1987/chtn_chtn_solution.cpp
@@ -0,0 +1,35 @@
+// Problem: chtn
+// Contest: 1987
+// Submission id: 389711710
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+int32_t main() {
+    
+    int tt;
+    cin >> tt;
+    while(tt--){
+        
+        int n;
+        cin >> n;
+    
+        vector<int> h(n);
+    
+        for (int i = 0; i < n; i++) {
+            cin >> h[i];
+        }
+    
+        int time = h[n - 1];
+    
+        for (int i = n - 2; i >= 0; i--) {
+            time = max(h[i], time + 1);
+        }
+    
+        cout << time << '\n';
+    }
+
+    return 0;
+}
