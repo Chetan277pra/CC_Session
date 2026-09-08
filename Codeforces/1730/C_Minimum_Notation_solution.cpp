adb432eed001c144d6164a26843e77c9d912997f
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-08 22:44:20 +0530

    Add Minimum Notation [C]

--- Codeforces/1730/C_Minimum_Notation_solution.cpp
+++ Codeforces/1730/C_Minimum_Notation_solution.cpp
@@ -5,33 +5,33 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
+ 
     int t;
     cin >> t;
-
+ 
     while (t--) {
         string s;
         cin >> s;
-
+ 
         int n = s.size();
         string a;
         int mn = 9;
-
+ 
         for (int i = n - 1; i >= 0; i--) {
             int x = s[i] - '0';
-
+ 
             if (x > mn)
                 a += char('0' + min(x + 1, 9));
             else
                 a += s[i];
-
+ 
             mn = min(mn, x);
         }
-
+ 
         sort(a.begin(), a.end());
         cout << a << '\n';
     }
