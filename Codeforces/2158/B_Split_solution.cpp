7fd3e7281cf62e98a6707f748709bf9998b84c7f
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-09 23:58:48 +0530

    Add Split [B]

--- Codeforces/2158/B_Split_solution.cpp
+++ Codeforces/2158/B_Split_solution.cpp
@@ -5,39 +5,39 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
+ 
     int t;
     cin >> t;
-
+ 
     while (t--) {
         int n;
         cin >> n;
-
+ 
         vector<int> cnt(2 * n + 1);
-
+ 
         for (int i = 0; i < 2 * n; i++) {
             int x;
             cin >> x;
             cnt[x]++;
         }
-
+ 
         int odd = 0, even = 0;
-
+ 
         for (int x = 1; x <= 2 * n; x++) {
             if (cnt[x] == 0) continue;
-
+ 
             if (cnt[x] & 1)
                 odd++;
             else
                 even++;
         }
-
+ 
         int ans;
-
+ 
         if (odd > 0) {
             ans = odd + 2 * even;
         } else {
@@ -46,9 +46,9 @@ int main() {
                 k--;
             ans = 2 * k;
         }
-
+ 
         cout << ans << '\n';
     }
-
+ 
     return 0;
 }
