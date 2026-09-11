f49a7f0d74d3b2d8ddbc69e582fa8e23b7c4ba34
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-11 21:43:15 +0530

    Add A Balanced Problemset? [B]

---
+++ Codeforces/1925/B_A_Balanced_Problemset_solution.cpp
@@ -0,0 +1,36 @@
+// Problem: A Balanced Problemset?
+// Contest: 1925
+// Link: https://codeforces.com/contest/1925/problem/B
+// Submission id: 390331518
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int x, n;
+        cin >> x >> n;
+
+        int ans = 1;
+
+        for (int i = 1; i * i <= x; i++) {
+            if (x % i == 0) {
+                if (i <= x / n)
+                    ans = max(ans, i);
+
+                if (x / i <= x / n)
+                    ans = max(ans, x / i);
+            }
+        }
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
