32358fac372914c9b21b0fe4db9f66c0c4033b02
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-13 14:06:46 +0530

    Add Test of Love [D]

--- Codeforces/1992/D_Test_of_Love_solution.cpp
+++ Codeforces/1992/D_Test_of_Love_solution.cpp
@@ -5,31 +5,31 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
+ 
     int t;
     cin >> t;
-
+ 
     while (t--) {
         int n, m, k;
         cin >> n >> m >> k;
-
+ 
         string s;
         cin >> s;
-
+ 
         s = "L" + s + "L";
         n += 2;
-
+ 
         int i = 0;
         bool ok = true;
-
+ 
         while (i < n - 1) {
             if (s[i] == 'L') {
                 bool f = false;
-
+ 
                 for (int j = i + 1; j < n && j <= i + m; j++) {
                     if (s[j] == 'L') {
                         i = j;
@@ -37,10 +37,10 @@ int main() {
                         break;
                     }
                 }
-
+ 
                 if (!f) {
                     i += m;
-
+ 
                     if (s[i] == 'C') {
                         ok = false;
                         break;
@@ -48,18 +48,18 @@ int main() {
                 }
             } else {
                 i++;
-
+ 
                 if (s[i] == 'C') {
                     ok = false;
                     break;
                 }
-
+ 
                 k--;
             }
         }
-
+ 
         cout << (ok && k >= 0 ? "YES" : "NO") << '\n';
     }
-
+ 
     return 0;
 }
