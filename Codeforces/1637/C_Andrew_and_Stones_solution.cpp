9b5f99adefa6fba2453186683b22e7f768e9faf6
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-15 20:54:46 +0530

    Add Andrew and Stones [C]

--- Codeforces/1637/C_Andrew_and_Stones_solution.cpp
+++ Codeforces/1637/C_Andrew_and_Stones_solution.cpp
@@ -5,38 +5,38 @@
 
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
- 
+
     int t;
     cin >> t;
- 
+
     while (t--) {
         int n;
         cin >> n;
- 
+
         vector<long long> a(n);
- 
+
         for (auto &x : a)
             cin >> x;
- 
+
         bool big = false;
         long long ans = 0;
- 
+
         for (int i = 1; i < n - 1; i++) {
             big |= (a[i] > 1);
             ans += (a[i] + 1) / 2;
         }
- 
+
         if (!big || (n == 3 && a[1] % 2 == 1)) {
             cout << -1 << '\n';
             continue;
         }
- 
+
         cout << ans << '\n';
     }
- 
+
     return 0;
 }
