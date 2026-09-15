96253f69704da0f283924e7e37aace7b817737c0
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-15 21:07:50 +0530

    Add Even-Odd Game [D]

---
+++ Codeforces/1472/D_Even-Odd_Game_solution.cpp
@@ -0,0 +1,40 @@
+// Problem: Even-Odd Game
+// Contest: 1472
+// Link: https://codeforces.com/contest/1472/problem/D
+// Submission id: 390842562
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        vector<int> a(n);
+        for (int &x : a) cin >> x;
+ 
+        sort(a.rbegin(), a.rend());
+ 
+        long long x = 0, y = 0;
+ 
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0 && a[i] % 2 == 0)
+                x += a[i];
+            else if (i % 2 == 1 && a[i] % 2 == 1)
+                y += a[i];
+        }
+ 
+        if (x > y) cout << "Alice\n";
+        else if (x < y) cout << "Bob\n";
+        else cout << "Tie\n";
+    }
+ 
+    return 0;
+}
