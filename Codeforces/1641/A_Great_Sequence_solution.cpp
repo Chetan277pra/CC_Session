de054403347be991ddd3a2909fdc30a9d3c487e8
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-17 10:26:44 +0530

    Add Great Sequence [A]

---
+++ Codeforces/1641/A_Great_Sequence_solution.cpp
@@ -0,0 +1,33 @@
+// Problem: Great Sequence
+// Contest: 1641
+// Link: https://codeforces.com/contest/1641/problem/A
+// Submission id: 390984152
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main() 
+{
+    int tt;
+    cin >> tt;
+    while(tt--){
+        int n , x;
+        cin >> n >> x;
+        vector<int> vec(n);
+        for(auto & a : vec) cin >> a;
+        map<int , int> mpp;
+        for(auto a : vec) mpp[a]++;
+        sort(vec.begin() , vec.end());
+        int count = 0;
+        for(auto a : vec){
+            if(mpp[a] == 0) continue;
+            if(mpp[a*x] != 0) {mpp[a*x]--; mpp[a]--; continue; }
+            mpp[a]--;
+            count++;
+            // cout << a << " " << mpp[a] << " " << a*x << " " << mpp[a*x] << endl;
+        }
+        cout << count << "\n";
+        
+    }
+    return 0;
+}
