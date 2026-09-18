7a0c99741e056f512edca1ea0a7229aa7bcb1c25
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-18 21:11:17 +0530

    Add A-B Palindrome [C]

--- Codeforces/1512/C_A-B_Palindrome_solution.cpp
+++ Codeforces/1512/C_A-B_Palindrome_solution.cpp
@@ -3,22 +3,87 @@
 // Link: https://codeforces.com/contest/1512/problem/C
 // Submission id: 391058559
 
-9
-4 4
-01?????0
-3 3
-??????
-1 0
-?
-2 2
-0101
-2 2
-01?0
-0 1
-0
-0 3
-1?1
-2 2
-?00?
-4 3
-??010?0
+#include <iostream>
+using namespace std;
+
+int main() {
+    int tt;
+    cin >> tt;
+
+    while (tt--) {
+        int a, b;
+        cin >> a >> b;
+
+        string s;
+        cin >> s;
+
+        int n = s.length();
+
+        for (int i = 0; i < n / 2; i++) {
+            int j = n - i - 1;
+
+            if (s[i] != '?' && s[j] != '?' && s[i] != s[j]) {
+                cout << -1 << '\n';
+                goto next;
+            }
+
+            if (s[i] == '?')
+                s[i] = s[j];
+
+            else if (s[j] == '?')
+                s[j] = s[i];
+        }
+
+        for (char c : s) {
+            if (c == '0')
+                a--;
+            else if (c == '1')
+                b--;
+        }
+
+        if (a < 0 || b < 0) {
+            cout << -1 << '\n';
+            goto next;
+        }
+
+        if (n % 2 == 1 && s[n / 2] == '?') {
+            if (a % 2 == 1) {
+                s[n / 2] = '0';
+                a--;
+            }
+            else if (b % 2 == 1) {
+                s[n / 2] = '1';
+                b--;
+            }
+        }
+
+        if (a % 2 != 0 || b % 2 != 0) {
+            cout << -1 << '\n';
+            goto next;
+        }
+
+        for (int i = 0; i < n / 2; i++) {
+            int j = n - i - 1;
+
+            if (s[i] == '?' && s[j] == '?') {
+                if (a >= 2) {
+                    s[i] = s[j] = '0';
+                    a -= 2;
+                }
+                else if (b >= 2) {
+                    s[i] = s[j] = '1';
+                    b -= 2;
+                }
+            }
+        }
+
+        if (a != 0 || b != 0)
+            cout << -1 << '\n';
+        else
+            cout << s << '\n';
+
+        next:;
+    }
+
+    return 0;
+}
