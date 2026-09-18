921d80bfd6fc70e91aa37006ed134a95ee51791f
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-18 21:25:48 +0530

    Add Make It Good [C]

---
+++ Codeforces/1385/C_Make_It_Good_solution.cpp
@@ -0,0 +1,36 @@
+// Problem: Make It Good
+// Contest: 1385
+// Link: https://codeforces.com/contest/1385/problem/C
+// Submission id: 391150462
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        vector<int> a(n);
+        for (int &x : a)
+            cin >> x;
+ 
+        int pos = n - 1;
+ 
+        while (pos > 0 && a[pos - 1] >= a[pos])
+            pos--;
+ 
+        while (pos > 0 && a[pos - 1] <= a[pos])
+            pos--;
+ 
+        cout << pos << '\n';
+    }
+ 
+    return 0;
+}
