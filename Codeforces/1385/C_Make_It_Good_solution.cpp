948e6849213cdece6c52bfa4241eaa3bd480624f
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-19 21:34:16 +0530

    Add Make It Good [C]

--- Codeforces/1385/C_Make_It_Good_solution.cpp
+++ Codeforces/1385/C_Make_It_Good_solution.cpp
@@ -5,32 +5,32 @@
 
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
- 
+
     int t;
     cin >> t;
- 
+
     while (t--) {
         int n;
         cin >> n;
- 
+
         vector<int> a(n);
         for (int &x : a)
             cin >> x;
- 
+
         int pos = n - 1;
- 
+
         while (pos > 0 && a[pos - 1] >= a[pos])
             pos--;
- 
+
         while (pos > 0 && a[pos - 1] <= a[pos])
             pos--;
- 
+
         cout << pos << '\n';
     }
- 
+
     return 0;
 }
