050ed73627d5941686de8a74c166e12d0068a30c
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-19 22:14:25 +0530

    Add chtn

---
+++ Codeforces/1982/chtn_chtn_solution.cpp
@@ -0,0 +1,45 @@
+// Problem: chtn
+// Contest: 1982
+// Submission id: 391252105
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        long long l, r;
+        cin >> n >> l >> r;
+
+        vector<long long> a(n);
+        for (auto &x : a) cin >> x;
+
+        long long sum = 0;
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < n; right++) {
+            sum += a[right];
+
+            while (sum > r) {
+                sum -= a[left++];
+            }
+
+            if (sum >= l) {
+                ans++;
+                sum = 0;
+                left = right + 1;
+            }
+        }
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
