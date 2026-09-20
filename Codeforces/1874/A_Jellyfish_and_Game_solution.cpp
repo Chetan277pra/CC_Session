091a5a19bd9be297b0b822a6c27c02c403c9467e
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-20 22:38:45 +0530

    Add Jellyfish and Game [A]

---
+++ Codeforces/1874/A_Jellyfish_and_Game_solution.cpp
@@ -0,0 +1,48 @@
+// Problem: Jellyfish and Game
+// Contest: 1874
+// Link: https://codeforces.com/contest/1874/problem/A
+// Submission id: 391351169
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+int32_t main() {
+    int tt;
+    cin >> tt;
+
+    while (tt--) {
+        int n, m, k;
+        cin >> n >> m >> k;
+
+        vector<int> A(n), B(m);
+
+        for (auto &a : A) cin >> a;
+        for (auto &b : B) cin >> b;
+
+        if (k > 10) {
+            k -= (k - 10) / 2 * 2;
+        }
+
+        for (int it = 0; it < k; it++) {
+            sort(A.begin(), A.end());
+            sort(B.begin(), B.end());
+
+            if (it % 2 == 0) {
+                if (A[0] < B.back()) {
+                    swap(A[0], B.back());
+                }
+            }
+            else {
+                if (B[0] < A.back()) {
+                    swap(B[0], A.back());
+                }
+            }
+        }
+
+        cout << accumulate(A.begin(), A.end(), 0LL) << '\n';
+    }
+
+    return 0;
+}
