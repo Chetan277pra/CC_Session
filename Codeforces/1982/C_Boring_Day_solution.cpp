73bb3656c0710919b2e653f3d51ded93731434c1
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-20 21:56:47 +0530

    Add Boring Day [C]

--- Codeforces/1982/C_Boring_Day_solution.cpp
+++ Codeforces/1982/C_Boring_Day_solution.cpp
@@ -5,42 +5,42 @@
 
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
- 
+
     int t;
     cin >> t;
- 
+
     while (t--) {
         int n;
         long long l, r;
         cin >> n >> l >> r;
- 
+
         vector<long long> a(n);
         for (auto &x : a) cin >> x;
- 
+
         long long sum = 0;
         int left = 0;
         int ans = 0;
- 
+
         for (int right = 0; right < n; right++) {
             sum += a[right];
- 
+
             while (sum > r) {
                 sum -= a[left++];
             }
- 
+
             if (sum >= l) {
                 ans++;
                 sum = 0;
                 left = right + 1;
             }
         }
- 
+
         cout << ans << '\n';
     }
- 
+
     return 0;
 }
