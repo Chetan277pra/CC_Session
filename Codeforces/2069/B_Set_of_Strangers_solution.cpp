65e148315b02b922c52427e01fb6529ded909377
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-22 22:01:37 +0530

    Add Set of Strangers [B]

--- Codeforces/2069/B_Set_of_Strangers_solution.cpp
+++ Codeforces/2069/B_Set_of_Strangers_solution.cpp
@@ -5,47 +5,47 @@
 
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
- 
+
     int t;
     cin >> t;
- 
+
     while (t--) {
         int n, m;
         cin >> n >> m;
- 
+
         vector<vector<int>> a(n, vector<int>(m));
         vector<int> cost(n * m + 1, 0);
- 
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 cin >> a[i][j];
                 cost[a[i][j]] = 1;
             }
         }
- 
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (i + 1 < n && a[i][j] == a[i + 1][j])
                     cost[a[i][j]] = 2;
- 
+
                 if (j + 1 < m && a[i][j] == a[i][j + 1])
                     cost[a[i][j]] = 2;
             }
         }
- 
+
         int total = 0, mx = 0;
- 
+
         for (int c = 1; c <= n * m; c++) {
             total += cost[c];
             mx = max(mx, cost[c]);
         }
- 
+
         cout << total - mx << '\n';
     }
- 
+
     return 0;
 }
