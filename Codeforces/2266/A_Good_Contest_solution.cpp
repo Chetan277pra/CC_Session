cd43d375cd356489b825b42171cb7946d7887f23
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-22 22:08:35 +0530

    Add Good Contest [A]

---
+++ Codeforces/2266/A_Good_Contest_solution.cpp
@@ -0,0 +1,24 @@
+// Problem: Good Contest
+// Contest: 2266
+// Link: https://codeforces.com/contest/2266/problem/A
+// Submission id: 391689155
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int tt;
+    cin >> tt;
+    while(tt--){
+        int n;
+        cin >> n;
+        int mini = INT_MAX;
+        for(int i = 0; i < 3; i++){
+            int temp; cin >> temp;
+            mini = min(mini , temp);
+        }
+        cout << n - mini <<"\n";
+    }
+    return 0;
+}
