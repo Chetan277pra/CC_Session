69e6faa2e3f6c19e3a780b7ae2ce3930d372a8bd
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-24 18:21:40 +0530

    Add Addition on a Segment [B]

---
+++ Codeforces/2170/B_Addition_on_a_Segment_solution.cpp
@@ -0,0 +1,33 @@
+// Problem: Addition on a Segment
+// Contest: 2170
+// Link: https://codeforces.com/contest/2170/problem/B
+// Submission id: 391876690
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+ 
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n, s = 0, c = 0;
+        cin >> n;
+ 
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            s += x;
+            if (x) c++;
+        }
+ 
+        cout << min(c, s - n + 1) << '\n';
+    }
+ 
+    return 0;
+}
